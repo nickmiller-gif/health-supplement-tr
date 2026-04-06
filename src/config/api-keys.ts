9b7bcb75b4d3dc7e537a41585b9f2c8d425ab996
nickmiller-gif <nick.miller@tacendalaw.com>
2026-04-06 01:17:57 +0000

    Generated by Spark: Fix all reported errors.

--- src/config/api-keys.ts
+++ src/config/api-keys.ts
@@ -46,5 +46,9 @@
 
 export const API_KEYS = {
   exa: '8805b859-b175-4097-9ceb-eb424e40ef0e',
-  rapidApi: 'fbd977f6d6msha78628ae99e0e78p18c74cjsne54b63bfded9'
+  rapidApi: 'fbd977f6d6msha78628ae99e0e78p18c74cjsne54b63bfded9',
+  reddit: {
+    clientId: '',
+    clientSecret: ''
+  }
 }
